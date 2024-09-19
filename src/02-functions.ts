cd9bf2e6767a8feb582ad6bf3dc5d207f2451a45
dlaw4608 <dlawton032@gmail.com>
2024-09-19 14:06:34 +0100

    Typed function signature.
    Signed-off-by: dlaw4608 <dlawton032@gmail.com>

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,44 @@
+import { friends, colleagues } from './01-basics'
 import {Friend, Colleague } from './myTypes'
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}`
+}
+
+console.log(older(friends[0]))
+
+function allOlder(f: Friend[]) {
+    let j : string[] = []
+    for(let i = 0; i < f.length; i++) {
+        f[i].age += 1;
+
+        j.push(f[i].name + " is now " + f[i].age)
+    }
+    return j;
+}
+console.log(allOlder(friends))
+
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+        (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+function addColleague(cs: Colleague[], name: string, department: string, email:string){
+    let newColleague : Colleague = {
+        name: name,
+        department: department,
+        contact: {
+            email: email,
+            extension: highestExtension(cs).contact.extension +=1
+        }
+    }
+
+    cs.push(newColleague);
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
